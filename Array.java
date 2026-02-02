8dc24a787d7ec6a8f2f818fda42b82b5ad8c87e2
Bhavish kumar <Bhavishsindhi3@gmail.com>
2026-02-03 04:37:48 +0500

    Array creation 1D,2D,3D and jagged array input and printing through loops

---
+++ Array.java
@@ -0,0 +1,143 @@
+public class Array {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        // 1D Array
+        // int num[] = {3,7,2,4};
+        // printing value
+        // System.out.println(num[0]);
+        // changing value
+        // num[1] = 10;
+        // System.out.println(num[1]);
+        
+        // initialize empty array (0's array)
+        // int arr[] = new int[4];
+        // arr[0] = 1;
+        // arr[1] = 4;
+        // arr[2] = 5;
+        // arr[3] = 6;
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+
+        // printing array with loop
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+        // Multidimensional Array
+        // 3 rows and 4 cols
+        // int nums[][] = new int[3][4];
+        // for (int i = 0; i < 3; i++) {
+        //     for (int j = 0; j < 4; j++) {
+        //         nums[i][j] = i + j + 10;
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int random = (int)Math.random() * 10;
+        // asigning random values using Math.random() and priting 
+        // for (int i = 0; i < 3; i++) {
+        //     for (int j = 0; j < 4; j++) {
+        //         nums[i][j] = (int)(Math.random() * 10);
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+        // System.out.println();
+        // System.out.println();
+        // System.out.println();
+
+
+        // another way to iterate array
+        // for (int n[] :nums) {
+        //     for(int m: n){
+        //         System.out.print(m + " ");
+        //     }
+        //     System.out.println();   
+        // }
+
+
+
+        // 3 Dimentional Array
+         
+        // int arrr[][][] = new int[3][4][5];
+        // int value = 1;
+        // for (int i = 0; i < arrr.length; i++) {
+        //     for (int j = 0; j < arrr[i].length; j++) {
+        //         for (int k = 0; k < arrr[i][j].length; k++) {
+        //             arrr[i][j][k] = value++;
+        //         }
+        //     }
+        // }
+        // for( int arrs[][]: arrr){
+        //     for(int arr[]:arrs){
+        //         for(int a:arr){
+
+        //             System.out.print(a + " ");
+
+        //         }
+        //         System.out.println();
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = 0; i < arrr.length; i++) {
+        //     System.out.println("Layer " + i);
+        //     for (int j = 0; j < arrr[i].length; j++) {
+        //         for (int k = 0; k < arrr[i][j].length; k++) {
+        //             System.out.print(
+        //                 "arrr[" + i + "][" + j + "][" + k + "] = " + arrr[i][j][k] + "   "
+        //             );
+        //         }
+        //         System.out.println();
+        //     }
+        //     System.out.println("---------------");
+        // }
+
+
+
+
+
+        // Jagged Array
+
+        // int jagged[][] = new int[3][]; // jagged array
+
+        // jagged[0]= new int[3];
+        // jagged[1]= new int[4];
+        // jagged[2]= new int[2];
+
+        // for (int i = 0; i < jagged.length; i++) {
+        //     for (int j = 0; j < jagged[i].length; j++) {
+        //         jagged[i][j] = (int)(Math.random() * 10);
+        //         System.out.print(jagged[i][j] + " ");
+        //     }
+        //     System.out.println();
+            
+        // }
+        // System.out.println();
+        // System.out.println();
+        // System.out.println();
+        // System.out.println();
+        
+        // // another way to iterate the array
+        
+        // for(int n[]: jagged){
+        //     for(int  m: n){
+        //         System.out.print(m + " ");
+
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        // Drawbacks of array
+
+        int arrs[] new int[3];
+    }
+}
