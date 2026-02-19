76a89865b81f8359d64e3dca4bbc396f9f5b42e5
Bhavish kumar <Bhavishsindhi3@gmail.com>
2026-02-20 00:12:38 +0500

    Completed the core java

--- StreamAPI/Implementation.java
+++ StreamAPI/Implementation.java
@@ -46,10 +46,16 @@ public class Implementation {
 
         // Stream API usage
         // Once we use it then we can't use it again
-        Stream<Integer> s1 = nums.stream();
-        Stream<Integer> s2 = s1.filter(n -> n%2 ==0);
-        Stream<Integer> s3 = s2.map(n -> n*2);
-        int result = s3.reduce(0,(c,e)-> c+e);
+        // Stream<Integer> s1 = nums.stream();
+        // Stream<Integer> s2 = s1.filter(n -> n%2 ==0);
+        // Stream<Integer> s3 = s2.map(n -> n*2);
+
+        // Above in one line
+        int result = nums.stream()
+        .filter(n-> n%2==0)
+        .map(n->n*2)
+        .reduce(0,(c,e)-> c+e);
+        // int result = s3.reduce(0,(c,e)-> c+e);
         System.out.println(result);
         // s3.forEach(n -> System.out.println(n));
         // s1.forEach(n -> System.out.println(n));
