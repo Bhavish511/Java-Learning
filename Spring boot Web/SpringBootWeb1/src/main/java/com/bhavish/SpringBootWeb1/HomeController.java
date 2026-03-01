e58ad9cbdb7d5140652f9f1ab009f317540084a3
Bhavish kumar <Bhavishsindhi3@gmail.com>
2026-03-02 03:45:30 +0500

    Handling with ModelAndView Object

--- Spring boot Web/SpringBootWeb1/src/main/java/com/bhavish/SpringBootWeb1/HomeController.java
+++ Spring boot Web/SpringBootWeb1/src/main/java/com/bhavish/SpringBootWeb1/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -48,17 +49,20 @@ public class HomeController {
     @RequestMapping("add")
     //public String add(int num1,int num2, HttpSession session){
     //OR
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
 
         //    int num1 = Integer.parseInt(req.getParameter("num1"));
         //    int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
-        model.addAttribute("result",result);
+//        model.addAttribute("result",result);
+        mv.addObject("result",result);
+        mv.setViewName("result");
 //        session.setAttribute("result",result);
         //        System.out.println(result);
         System.out.println("Called Add");
 //        return "result.jsp";
-        return "result";
+//        return "result";
+        return mv;
 
     }
 }
